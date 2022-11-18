7ef71d9ac53730e266f747d18cfa65ba11888ce3
Jose-Medina-web <josemedinamunoz303@gmail.com>
2022-11-18 10:04:03 -0500

    cambio de ubicacion carpetas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +21,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
 
 Route::resource('/dashboard',DashboardController::class)
 ->names('dashboard');
