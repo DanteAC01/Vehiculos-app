9f38581167dc4fa843541206dd51131072e4ba3e
0elizabeth <ferchipinzonh@gmail.com>
2022-11-22 10:47:34 -0500

    Autos

---
+++ database/migrations/2022_11_22_151456_create_autos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('marca_id');
+            $table->string('modelo');
+            $table->string('estado');
+            $table->decimal('precio', 8, 2);
+            $table->decimal('kilometraje', 8, 2);
+            $table->unsignedBigInteger('user_id');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('autos');
+    }
+}
